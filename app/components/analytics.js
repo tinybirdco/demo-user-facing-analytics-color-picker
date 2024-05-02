5b9e82f37166ce4f71dac0c12ca2e92d5cb86c68
tb-peregrine <cameronarcher@tinybird.co>
2024-05-02 13:12:21 -0500

    first component

--- app/components/analytics.js
+++ app/components/analytics.js
@@ -1,19 +1,14 @@
 'use client';
 
 import React from 'react';
-import GameTracker from './gameTracker';
 import FastestGame from './fastestGame';
-import FastestClick from './fastestClick';
-import FavoriteTarget from './favoriteTarget';
-import NemesisTarget from './nemesisTarget';
 import { Grid } from '@tremor/react';
-import Leaderboard from './leaderboard';
 require('dotenv').config({ path: '../.env.local'})
 
-const TINYBIRD_HOST = 'api.us-east.aws.tinybird.co';
-const TINYBIRD_TOKEN = 'p.eyJ1IjogIjBiMjJiMDkxLTdjNzktNDRlMS1hZTg1LTQwYjhhNmNiNTE1NyIsICJpZCI6ICJhNzdjYmIwNS1hNGYzLTRhNTEtYWEwNy1iNTI2NzViZGJkYmMiLCAiaG9zdCI6ICJ1cy1lYXN0LWF3cyJ9.3IFDsL5yBA8-Dg5srBixhfpXTmHr3q_li0aNQayKg5I';
+const TINYBIRD_HOST = process.env.TINYBIRD_HOST
+const TINYBIRD_TOKEN = process.env.TINYBIRD_TOKEN
 
-export default function Analytics({username, gameStarted, currentGameProgress}) { 
+export default function Analytics({username, gameStarted}) { 
     let host = TINYBIRD_HOST;
     let token = TINYBIRD_TOKEN;
 
@@ -27,38 +22,7 @@ export default function Analytics({username, gameStarted, currentGameProgress}) 
                     username={username}
                     gameStarted={gameStarted}
                 />
-                <FastestClick
-                    host={host}
-                    token={token}
-                    username={username}
-                    gameStarted={gameStarted}
-                />
-                <FavoriteTarget
-                    host={host}
-                    token={token}
-                    username={username}
-                    gameStarted={gameStarted}
-                />
-                <NemesisTarget
-                    host={host}
-                    token={token}
-                    username={username}
-                    gameStarted={gameStarted}
-                />
             </Grid>
-            <GameTracker
-                host={host}
-                token={token}
-                username={username}
-                gameStarted={gameStarted}
-                currentGameProgress={currentGameProgress}
-            />
-            <Leaderboard
-                host={host}
-                token={token}
-                username={username}
-                gameStarted={gameStarted}
-            />
         </div>
     )
 }
--- app/utils/tinybird.js
+++ app/utils/tinybird.js
@@ -1,25 +1,5 @@
-const gameTrackerUrl = (host, token, username) => {
-    return `https://${host}/v0/pipes/game_tracker.json?username=${username}&token=${token}`
-}
-
 const fastestGameUrl = (host, token, username) => {
     return `https://${host}/v0/pipes/fastest_game.json?username=${username}&token=${token}`
-}
-
-const fastestClickUrl = (host, token, username) => {
-    return `https://${host}/v0/pipes/fastest_click.json?username=${username}&token=${token}`
-}
-
-const favoriteTargetUrl = (host, token, username) => {
-    return `https://${host}/v0/pipes/favorite_target.json?username=${username}&token=${token}`
-}
-
-const nemesisTargetUrl = (host, token, username) => {
-    return `https://${host}/v0/pipes/nemesis_target.json?username=${username}&token=${token}`
-}
-
-const leaderboardUrl = (host, token) => {
-    return `https://${host}/v0/pipes/leaderboard.json?&token=${token}`
 }
 
 const fetchTinybirdApi = async (url, setData) => {
@@ -35,10 +15,5 @@ const fetchTinybirdApi = async (url, setData) => {
 
 export {
     fetchTinybirdApi,
-    gameTrackerUrl,
-    fastestGameUrl,
-    fastestClickUrl,
-    favoriteTargetUrl,
-    nemesisTargetUrl,
-    leaderboardUrl
+    fastestGameUrl
 }
