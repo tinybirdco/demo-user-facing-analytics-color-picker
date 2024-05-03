185740b8f3784f90135a8fd60d3f775d5d0dcd56
tb-peregrine <cameronarcher@tinybird.co>
2024-05-03 10:02:38 -0500

    Tinybird tokens on proxy

--- app/components/analytics.js
+++ app/components/analytics.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FastestGame from './fastestGame';
 import { Grid } from '@tremor/react';
 
-const HOST = 'api.us-east.aws.tinybird.co';
-const TOKEN = 'p.eyJ1IjogIjBiMjJiMDkxLTdjNzktNDRlMS1hZTg1LTQwYjhhNmNiNTE1NyIsICJpZCI6ICJhNzdjYmIwNS1hNGYzLTRhNTEtYWEwNy1iNTI2NzViZGJkYmMiLCAiaG9zdCI6ICJ1cy1lYXN0LWF3cyJ9.3IFDsL5yBA8-Dg5srBixhfpXTmHr3q_li0aNQayKg5I';
-
 export default function Analytics({username, gameStarted}) {
+
+    // Set Tinybird auth states
+    const [tinybirdHost, setTinybirdHost] = useState('');
+    const [tinybirdToken, setTinybirdToken] = useState('');
+
+    // Fetch tokens from the proxy
+    useEffect(() => {
+        fetch('http://localhost:3001/api/tinybird')
+            .then(response => response.json())
+            .then(data => {
+                const { TINYBIRD_HOST, TINYBIRD_TOKEN } = data;
+                setTinybirdHost(TINYBIRD_HOST);
+                setTinybirdToken(TINYBIRD_TOKEN);
+            })
+            .catch(error => console.error('Error fetching Tinybird env variables: ', error));
+    }, []);
+
     return (
         <div className='analytics-container'>
             <h2>Analytics for {username}</h2>
             <Grid className='grid-cols-4 gap-6'>
                 <FastestGame
-                    host={HOST}
-                    token={TOKEN}
+                    host={tinybirdHost}
+                    token={tinybirdToken}
                     username={username}
                     gameStarted={gameStarted}
                 />
